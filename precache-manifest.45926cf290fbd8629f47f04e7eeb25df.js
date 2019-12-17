d3c5a4e82a20aad364dfde5e6de0e4f3bba9652b
shwetakelkar <shweta4.gupta@gmail.com>
2019-12-17 12:36:06 -0600

    Updates

---
+++ precache-manifest.45926cf290fbd8629f47f04e7eeb25df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3af044309f4c3f0decaa86c7cce5682",
+    "url": "/game-clicky/index.html"
+  },
+  {
+    "revision": "9f942a6d197483ba30f8",
+    "url": "/game-clicky/static/css/main.3fc91a95.chunk.css"
+  },
+  {
+    "revision": "1a41132f2ddc67996686",
+    "url": "/game-clicky/static/js/2.253d3936.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/game-clicky/static/js/2.253d3936.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9f942a6d197483ba30f8",
+    "url": "/game-clicky/static/js/main.be58cddb.chunk.js"
+  },
+  {
+    "revision": "1f8ea85a40a0cfe5f429",
+    "url": "/game-clicky/static/js/runtime-main.0676c096.js"
+  }
+]);
